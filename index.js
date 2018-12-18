e51732b700c707e31e7bb319551e1eceb2ea21ee
Brianna Dixon <brianna_dixon@rocketmail.com>
2018-12-18 14:30:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ var recipes = { vodka: '2.5 oz', oliveBrine: '.5 oz', vermouth: '4 dashes'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, {recipes}, {olive: 'to garnish'}); 
-  return recipes
+  return recipes;
 }
+
+
+
