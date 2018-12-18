e67b5d65b56ee48c3e34ef306b04da8bbbbd8204
Brianna Dixon <brianna_dixon@rocketmail.com>
2018-12-18 14:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete recipes.prop;
-  return recipes;
+  delete object.key;
+  return object;
 }
 
 
